5e305967b88ee22c7d8205b0716b1b50c167ca5a
Togarci <thyago.garcia10@gmail.com>
2022-07-04 21:24:16 -0300

    se aplicado este commit ira implementar o layout da home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
+import BannerComplete from '../assets/img/BannerComplete.png';
+
 export default function HomePage() {
     return (
         <div id="home-page">
-            
+            <div className="d-flex justify-content-between">
+                <section className="col-4">
+                    <h2>Qual pokemon você <span>escolheria</span>?</h2>
+                    
+                    <p>Você pode saber o tipo de Pokémon, seus pontos fortes, fracos e habilidades.</p>
+                    <button className="primary-btn">Veja os pokemons</button>
+                </section>
+                <div className="">
+                    <img src={BannerComplete} />
+                </div>
+            </div>
         </div>
     )
 }
